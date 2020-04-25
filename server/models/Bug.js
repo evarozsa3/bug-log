46e9a30f3291863ce138a2513b03f097a1eb44d3
Eva Rozsa <Eva Rozsa>
2020-04-25 01:23:40 -0600

    ok hope this works ok

---
+++ server/models/Bug.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose"
+import { dbContext } from "../db/DbContext"
+let Schema = mongoose.Schema
+let ObjectId = Schema.Types.ObjectId
+
+const Bug = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  creatorEmail: { type: String, required: true }
+}, { timestamps: true, toJSON: { virtuals: true } })
+
+Bug.virtual("creator",
+  {
+    localField: "creatorEmail",
+    ref: "Profile",
+    foreignField: "email",
+    justOne: true
+  })
+
+//CASCADE ON DELETE
+//Board.pre('findOneAndRemove', function (next) {
+//  //lets find all the lists and remove them
+//  Promise.all([
+//    // @ts-ignore
+//    dbContext.Lists.deleteMany({ boardId: this._conditions._id })
+//  ])
+//    .then(() => next())
+//    .catch(err => next(err))
+//})
+
+export default Bug
---
+++ server/services/BugService.js
@@ -0,0 +1,49 @@
+import { dbContext } from "../db/DbContext"
+import { BadRequest } from "../utils/Errors"
+
+
+class BugService {
+  async getAll(userEmail) {
+    return await dbContext.Bugs.find({ creatorEmail: userEmail }).populate("creator", "name picture")
+  }
+
+  async getById(id, userEmail) {
+    let data = await dbContext.Bugs.findById({ _id: id, creatorEmail: userEmail })
+    if (!data) {
+      throw new BadRequest("Invalid ID or you do not own this board")
+    }
+    return data
+  }
+
+  // async getBoardListsByBoardId(boardId, userEmail) {
+  //   let data = await dbContext.Notes.find({ boardId: boardId, creatorEmail: userEmail })
+  //   if (!data) {
+  //     throw new BadRequest("Invalid ID or you do not own this board")
+  //   }
+  //   return data
+  // }
+
+  async create(rawData) {
+    let data = await dbContext.Bugs.create(rawData)
+    return data
+  }
+
+  async edit(id, userEmail, update) {
+    let data = await dbContext.Bugs.findOneAndUpdate({ _id: id, creatorEmail: userEmail }, update, { new: true })
+    if (!data) {
+      throw new BadRequest("Invalid ID or you do not own this board");
+    }
+    return data;
+  }
+
+  async delete(id, userEmail) {
+    let data = await dbContext.Bugs.findOneAndRemove({ _id: id, creatorEmail: userEmail });
+    if (!data) {
+      throw new BadRequest("Invalid ID or you do not own this board");
+    }
+  }
+
+}
+
+
+export const bugService = new BugService()
